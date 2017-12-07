7ef6c07c6668c9e2a58c88ac8d253373718dd9ac
Petr Reshetin <preshetin@gmail.com>
2017-12-07 21:48:57 +0300

    Formula for our plan

--- src/App.js
+++ src/App.js
@@ -9,13 +9,42 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rateOfReturn: 0.5
+      chartParams: {
+        initialInvestment: 75000,
+        pacMonth: 550,
+        pacMinusInsuranceCost: 470,
+        primaryCIAmount: 69000,
+        secondaryCIAmount: 92000,
+        primaryCICost: 33,
+        secondaryCICost: 47,
+        includePrimaryCIInsurace: true,
+        insculdSecondaryCIInsurance: true,
+        currentAgeYongest: 30,
+        currentAgeOldest: 65,
+        rateOfReturn: 0.05,
+        ourFees: 0.005,
+        theirFees: 0.023,
+        primaryIllness: 1,
+        secondaryIllness: 1,
+        illnessEventDate: 47
+      }
     }
+
     this.handleRateOfReturnChange = this.handleRateOfReturnChange.bind(this);
+    this.handleInitialInvestmentChange = this.handleInitialInvestmentChange.bind(this);
   }
 
   handleRateOfReturnChange(rateOfReturn) {
-    this.setState({ rateOfReturn });
+    let state = this.state;
+    state.chartParams.rateOfReturn = rateOfReturn;
+    this.setState(state);
+  }
+
+
+  handleInitialInvestmentChange(initialInvestment) {
+    let state = this.state;
+    state.chartParams.initialInvestment = initialInvestment;
+    this.setState(state);
   }
 
   render() {
@@ -24,8 +53,14 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Investments & Insurance Graph</h1>
         </header>
-        <Chart rateOfReturn={this.state.rateOfReturn} />
-        <Controls rateOfReturn={this.state.rateOfReturn} onRateOfReturnChange={this.handleRateOfReturnChange}  />
+        <Chart chartParams={this.state.chartParams} />
+        <Controls
+          rateOfReturn={this.state.chartParams.rateOfReturn}
+          onRateOfReturnChange={this.handleRateOfReturnChange}
+          initialInvestment={this.state.chartParams.initialInvestment}
+          onInitialInvestmentChange={this.handleInitialInvestmentChange}
+
+          />
       </div>
     );
   }
--- src/Controls.js
+++ src/Controls.js
@@ -5,17 +5,26 @@ class Controls extends React.Component {
   constructor(props) {
     super(props);
     this.handleRateOfReturnChange = this.handleRateOfReturnChange.bind(this);
+    this.handleInitialInvestmentChange = this.handleInitialInvestmentChange.bind(this);
   }
 
   handleRateOfReturnChange(e) {
     this.props.onRateOfReturnChange(e.target.value);
   }
 
+  handleInitialInvestmentChange(e) {
+    this.props.onInitialInvestmentChange(e.target.value);
+  }
+/*
+<label>Rate of Return</label>
+<input type="number" value={this.props.rateOfReturn} onChange={this.handleRateOfReturnChange}  placeholder="Rate of Return" />
+*/
   render() {
     return (
       <div>
-      <label>Rate of Return</label>
-      <input type="number" value={this.props.rateOfReturn} onChange={this.handleRateOfReturnChange}  placeholder="Rate of Return" />
+
+        <label>Initial Investment, $ </label>
+        <input type="number" value={this.props.initialInvestment} onChange={this.handleInitialInvestmentChange}  placeholder="Initial Investment" />
       </div>
     );
   }
