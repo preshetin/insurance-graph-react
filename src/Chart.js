52cc1c065cf69272b81a75ade1454c71f747feb1
Petr Reshetin <preshetin@gmail.com>
2017-12-08 11:52:22 +0300

    Mutual funds graph

--- src/Chart.js
+++ src/Chart.js
@@ -7,8 +7,7 @@ class Chart extends React.Component {
   render() {
 
     const data = prepareData(this.props.chartParams);
-    console.log(data);
-// <Line type="monotone" dataKey="mutualFunds" stroke="#8884d8" activeDot={{r: 8}}/>
+
     return (
       <LineChart width={600} height={300} data={data}
                   margin={{top: 5, right: 30, left: 20, bottom: 5}} className="Chart">
@@ -17,8 +16,8 @@ class Chart extends React.Component {
              <CartesianGrid strokeDasharray="3 3"/>
              <Tooltip/>
              <Legend />
-
              <Line type="monotone" dataKey="ourPlan" stroke="#82ca9d" />
+             <Line type="monotone" dataKey="mutualFunds" stroke="#8884d8" activeDot={{r: 8}}/>
       </LineChart>
     );
   }
--- src/prepareData.js
+++ src/prepareData.js
@@ -1,22 +1,22 @@
 const prepareData = (chartParams) => {
 
-  // let ages = Array.from(Array(65).keys()).slice(30);
-  let ages = [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64];
+  let ages = Array.from(Array(65).keys()).slice(30);
   let data = [];
 
   ages.forEach( age => {
     let ourPlan;
     let mutualFunds;
-    if (age == 30) {
+    if (age === 30) {
       ourPlan     = getPlanStart(chartParams);
       mutualFunds = getPlanStart(chartParams);
     } else {
-      ourPlan = getOurPlanContinue(age, data.slice(-1)[0], chartParams);
-      mutualFunds = 200 + age * (0.7 * chartParams.rateOfReturn);
+      ourPlan     = getOurPlanContinue(age, data.slice(-1)[0], chartParams);
+      mutualFunds = getMutualFundsContinue(age, data.slice(-1)[0], chartParams);
     }
 
     data.push({
       name: age,
+      age,
       ourPlan,
       mutualFunds
     })
@@ -26,49 +26,48 @@ const prepareData = (chartParams) => {
 }
 
 const getPlanStart = (chartParams) => {
-  return parseInt(chartParams.initialInvestment) + chartParams.pacMinusInsuranceCost * 12;
+  return Math.round(chartParams.initialInvestment + chartParams.pacMinusInsuranceCost * 12);
 }
 
-/*
-  chartParams: {
-    rateOfReturn: 0.5,
-    initialInvestment: 75000,
-    pacMonth: 550,
-    pacMinusInsuranceCost: 470,
-    primaryCIAmount: 69000,
-    secondaryCIAmount: 92000,
-    primaryCICost: 33,
-    secondaryCICost: 47,
-    includePrimaryCIInsurace: true,
-    insculdSecondaryCIInsurance: true,
-    currentAgeYongest: 30,
-    currentAgeOldest: 65,
-    rateOfReturn: 0.5,
-    ourFees: 0.5,
-    theirFees: 0.023,
-    primaryIllness: 1,
-    secondaryIllness: 1,
-    illnessEventDate: 47
-*/
 const getOurPlanContinue = (age, prevData, chartParams) => {
 
   let one = 0;
   let two = 0;
   let three = 0;
-  if (age < chartParams.currentAgeOldest) {
+  if (prevData.age < chartParams.currentAgeOldest) {
      one = prevData.ourPlan * (1 + chartParams.rateOfReturn - chartParams.ourFees) + 12 * chartParams.pacMinusInsuranceCost;
 
-    if (age == chartParams.illnessEventDate && !chartParams.includePrimaryCIInsurace) {
+    if (age === chartParams.illnessEventDate && !chartParams.includePrimaryCIInsurace) {
       two = chartParams.primaryIllness * chartParams.primaryCIAmount;
 
       if ( ! chartParams.insculdSecondaryCIInsurance) {
         three = chartParams.secondaryIllness * chartParams.secondaryCIAmount;
       }
     }
-
   }
 
-  let result = parseInt(one - two + three);
+  let result = Math.round(one - two + three);
+
+  if (result > 1000000000) {
+    result = 1000000000
+  }
+
+  return result;
+}
+
+const getMutualFundsContinue = (age, prevData, chartParams) => {
+  let one = 0;
+  let two = 0;
+
+  if (prevData.age < chartParams.currentAgeOldest) {
+    one = prevData.mutualFunds * (1 + chartParams.rateOfReturn - chartParams.theirFees) + chartParams.pacMinusInsuranceCost * 12;
+
+    if (age === chartParams.illnessEventDate) {
+      two = chartParams.primaryIllness * chartParams.primaryCIAmount + chartParams.secondaryIllness * chartParams.secondaryCIAmount;
+    }
+  }
+
+  let result = Math.round(one - two);
 
   if (result > 1000000000) {
     result = 1000000000
